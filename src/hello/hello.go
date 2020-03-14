7565d152ff6f11146d421ada83b8c0872029294a
zenaro <zenaro@debian>
2020-03-14 19:49:16 -0300

    Capitulo 2

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Olá Mundo")
+	nome := "João"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O comando escolhido foi", comando)
 }

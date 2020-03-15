e099a509778d6b2742cc4d8e9b7ed694e21108bd
Joao Zenaro <jv_zenaro@hotmail.com>
2020-03-14 21:02:20 -0300

    Capitulo 5

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 	exibeIntroducao()
@@ -51,12 +55,25 @@ func exibeIntroducao() {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://random-status-code.herokuapp.com/"
+	sites := []string{"https://random-status-code.herokuapp.com/", "https://alura.com.br/", "https://caelum.com.br/", "https://pagseguro.uol.com.br/"}
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+	fmt.Println("")
+}
+
+func testaSite(site string) {
 	response, _ := http.Get(site)
 	if response.StatusCode >= 200 && response.StatusCode < 300 {
-		fmt.Println("Site", site, "carregado com sucesso!")
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
 	} else {
-		fmt.Println("Site", site, "está com problemas. Status Code:", response.StatusCode)
+		fmt.Println("Site:", site, "está com problemas. Status Code:", response.StatusCode)
 	}
 }
 
